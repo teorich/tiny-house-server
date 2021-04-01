657efd21c0d9f51f7cd71bd1c6532ef2088b2fa6
Theo <theo.foncham@gmail.com>
2021-04-01 16:03:06 +0100

    add ant design css

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,16 +1,16 @@
 import { MongoClient } from 'mongodb';
 import { Database } from '../lib/types';
 
-// const user = process.env.DB_USER;
-// const userPassword = process.env.DB_USER_PASSWORD;
-// const cluster = process.env.DB_CLUSTER;
-// // const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const user = process.env.DB_USER;
+const userPassword = process.env.DB_USER_PASSWORD;
+const cluster = process.env.DB_CLUSTER;
+const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const url2 =
-  'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+// const url2 =
+//   'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 
 export const connectDatabase = async (): Promise<Database> => {
-  const client = await MongoClient.connect(url2, {
+  const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
